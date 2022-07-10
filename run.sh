d09fe13447098db0dda0435a31223f9d72d9faff
Trevor Knott <tknott95@hotmail.com>
2022-07-10 08:03:02 -0600

    Haskell Scraper v0.0.0

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # @TODO fix -threaded flag as stack changes flags
 
 # stack will not allow for some ghc flags, or idk the work around
- stack ghc colors.hs dl.hs main.hs  --rts-options -threaded  && rm -f *.hi && rm -f *.o && rm -f hreaded && ./main $1 -RTS -N4 +RTS -s 
+ stack ghc colors.hs dl.hs main.hs  --rts-options -threaded  -eventlog && rm -f *.hi && rm -f *.o && rm -f hreaded && ./main $1 -RTS -N4 +RTS -s 
